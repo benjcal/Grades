d8849f009cdb2a43b2dc11324336c05f83f916fc
Benjamin Calderon <benj.calderon@gmail.com>
2018-03-22 00:45:01 -0400

    experiments with source map and changed from importing lodash to just specific functions

--- src/store/reducers/activities.js
+++ src/store/reducers/activities.js
@@ -1,14 +1,18 @@
-import _ from 'lodash'
+import assign from 'lodash.assign'
+import set from 'lodash.set'
+import merge from 'lodash.merge'
+import omit from 'lodash.omit'
+
 import { activityIdToKey } from 'libs/utils'
 
 export default function activities(state = {}, action) {
     switch (action.type) {
         case 'ADD_ACTIVITY':
-            return _.assign(state, _.set({}, activityIdToKey(action.payload.id), action.payload))
+            return assign(state, set({}, activityIdToKey(action.payload.id), action.payload))
         case 'UPDATE_ACTIVITY':
-            return _.merge(state, _.set({}, activityIdToKey(action.payload.id), action.payload))
+            return merge(state, set({}, activityIdToKey(action.payload.id), action.payload))
         case 'REMOVE_ACTIVITY':
-            return _.omit(state, activityIdToKey(action.payload.id))
+            return omit(state, activityIdToKey(action.payload.id))
         default:
             return state
     }
